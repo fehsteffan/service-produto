8738cda66f07e3ec9a081f19af1eccbc55c51faf
Felipe Steffan <felip.developer@hotmail.com>
2022-11-14 21:00:04 -0300

    Criando metodo POST

--- src/main/java/com/program/produto/controllers/ProdutoController.java
+++ src/main/java/com/program/produto/controllers/ProdutoController.java
@@ -2,10 +2,12 @@ package com.program.produto.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.program.produto.dto.ProdutoDto;
+import com.program.produto.model.Produto;
 import com.program.produto.services.ProdutoService;
 
 @RestController
@@ -13,11 +15,22 @@ import com.program.produto.services.ProdutoService;
 public class ProdutoController {
 	
 	
+	@Autowired
+	private ProdutoService service;
+	
+	
+	
+	public ProdutoController(ProdutoService service) {		
+		this.service = service;
+	}
 
-	
-	
-	
-	
+
+
+	@PostMapping
+	public Produto insert(@RequestBody ProdutoDto dto) {
+		Produto produto = new Produto(1L, dto.getDescription(), dto.getValor());
+		return service.insert(produto);
+	}
 	
 
 }
